b9300082da7041b9c42ecf5fda55168ff98a283f
whyfari <whyfari@gmail.com>
2019-03-07 21:12:04 -0500

    #3

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 //    database: 'mongodb://localhost:27017/meanauth',
 // using the database hosted on 
-    database: 'mongodb+srv://mean:mean@farimongodbs-cykfn.mongodb.net/test?retryWrites=true',
+    database: 'mongodb+srv://mean:mean@farimongodbs-cykfn.mongodb.net/meanauth?retryWrites=true',
     secret: 'yoursecret'
 }
