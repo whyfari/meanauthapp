eb5dc8d7c1b1eac3f8e951e1736275bdaf5d7217
whyfari <whyfari@gmail.com>
2019-03-07 20:59:43 -0500

    commit 2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+//    database: 'mongodb://localhost:27017/meanauth',
+// using the database hosted on 
+    database: 'mongodb+srv://mean:mean@farimongodbs-cykfn.mongodb.net/test?retryWrites=true',
     secret: 'yoursecret'
 }
